da9e4c81620ffe79f8b40fb1d057580e0f7e6587
chrisfitkin <cfitkin@gmail.com>
2016-09-11 06:45:56 -0700

    reduce

---
+++ reduce.js
@@ -0,0 +1,9 @@
+function countWords(inputWords) {
+  // SOLUTION GOES HERE
+  return inputWords.reduce((previous, current, index, array) => {
+    previous[current] = (Number.isInteger(previous[current])) ? ++previous[current] : 1;
+    return previous;
+  }, {});
+}
+
+module.exports = countWords
