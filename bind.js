d1eeaa9dd2ba93a8188c9fec4c5ede1fd32d8e77
chrisfitkin <cfitkin@gmail.com>
2016-09-12 20:09:27 -0700

    clean up bind.js

--- bind.js
+++ bind.js
@@ -1,8 +1,3 @@
-var slice = Array.prototype.slice
-
 module.exports = function (namespace) {
-  // return function() {
-    // console.log.bind([namespace].concat(slice.call(arguments)));
-    return console.log.bind(null, namespace)
-  // }
+  console.log.bind(null, namespace);
 }
